71eb00e0a264f74b00660acf2092ff32a2d42aa7
unknown <Brijeshjha670>
2023-09-05 07:32:12 +0530

    chat_app_5sep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './Components/ChatFeed';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine 
+      height="100vh"
+      projectID="
+      ef74afad-482e-4a1c-90b7-966019499cec"
+      userName="john"
+      userSecret="bingbong@123%"
+      renderChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      onNewMessage = {() => new Audio('https://chat-engine-assets.s3.amazonawz.com/click.mp3')}
+      />
     </div>
   );
 }
